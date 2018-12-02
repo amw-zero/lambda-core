66872f9ba9d52e26d8468dbd860f4b41b9bed8ca
Alex Weisberger <alex.weisberger@viewthespace.com>
2018-12-01 20:10:56 -0500

    Add application layer framework

---
+++ LambdaCoreApplication/LambdaCoreApplication.h
@@ -0,0 +1,19 @@
+//
+//  LambdaCoreApplication.h
+//  LambdaCoreApplication
+//
+//  Created by Alex Weisberger on 12/1/18.
+//  Copyright © 2018 vts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LambdaCoreApplication.
+FOUNDATION_EXPORT double LambdaCoreApplicationVersionNumber;
+
+//! Project version string for LambdaCoreApplication.
+FOUNDATION_EXPORT const unsigned char LambdaCoreApplicationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LambdaCoreApplication/PublicHeader.h>
+
+
---
+++ LambdaCoreApplication/LoginOrchestrator.swift
@@ -0,0 +1,15 @@
+//
+//  LoginOrchestrator.swift
+//  LambdaCoreApplication
+//
+//  Created by Alex Weisberger on 12/1/18.
+//  Copyright © 2018 vts. All rights reserved.
+//
+
+import Foundation
+import LambdaCoreCore
+public struct LoginOrchestrator {
+    public init() {
+        let m = Model()
+    }
+}
