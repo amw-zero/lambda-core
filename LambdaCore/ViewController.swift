bcbc477c8b59c03a14c9bbca347b6e5f7500ded7
Alex Weisberger <alex.weisberger@viewthespace.com>
2018-12-01 20:01:59 -0500

    Initial commit

---
+++ LambdaCore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LambdaCore
+//
+//  Created by Alex Weisberger on 12/1/18.
+//  Copyright © 2018 vts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
