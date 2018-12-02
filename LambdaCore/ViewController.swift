91b1ba292d6e8cde66626dc2238c897d1cf96a95
Alex Weisberger <alex.weisberger@viewthespace.com>
2018-12-01 20:07:10 -0500

    Add core library

--- LambdaCore/ViewController.swift
+++ LambdaCore/ViewController.swift
@@ -7,11 +7,13 @@
 //
 
 import UIKit
+import LambdaCoreCore
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let m = Model()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
---
+++ LambdaCoreCore/Model.swift
@@ -0,0 +1,15 @@
+//
+//  Model.swift
+//  LambdaCoreCore
+//
+//  Created by Alex Weisberger on 12/1/18.
+//  Copyright © 2018 vts. All rights reserved.
+//
+
+import Foundation
+
+public struct Model {
+    public init() {
+        
+    }
+}
