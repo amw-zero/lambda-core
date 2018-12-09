3a8f5a16ccacf77e095d73a3ce0fe3823f38c19a
Alex Weisberger <alex.weisberger@viewthespace.com>
2018-12-09 00:23:27 -0500

    Attempt at making Orchestrators generic.

--- LambdaCoreApplication/LoginOrchestrator.swift
+++ LambdaCoreApplication/LoginOrchestrator.swift
@@ -10,28 +10,42 @@ import Foundation
 import LambdaCoreModel
 
 public protocol Executor {
-    func execute(withOrchestrator: LoginOrchestrator)
+    associatedtype UseCaseT: UseCase
+    associatedtype ExProducer: ExecutorProducer
+    func execute(withOrchestrator: LoginOrchestrator<UseCaseT, ExProducer>)
 }
 
 public protocol ExecutorProducer {
-    func executorFor(effect: Effect) -> Executor
+    func executorFor<T: Executor>(effect: Effect) -> T
 }
 
 public protocol Orchestratable {
-    var orchestrator: LoginOrchestrator! { get set }
+    associatedtype UseCaseT: UseCase
+    associatedtype ExProducer: ExecutorProducer
+    var orchestrator: LoginOrchestrator<UseCaseT, ExProducer>! { get set }
+}
+
+public protocol UseCase {
+    associatedtype State
+    associatedtype Action
+    func receive(_ action: Action, inState state: State) -> (State, Effect?)
 }
 
 // This can probably be made generic, a la the Elm runtime. Orchestrator<UseCase, UseCaseState>
-public class LoginOrchestrator {
-    let useCase: LoginUseCase = LoginUseCase()
-    var state: LoginState = LoginState()
-    public var onNewState: (LoginState) -> Void
-    let executorFactory: ExecutorProducer
-    public init(executorFactory: ExecutorProducer, onNewState: @escaping (LoginState) -> Void) {
+public class LoginOrchestrator<UseCaseT: UseCase, ExProducer: ExecutorProducer> {
+    public typealias State = UseCaseT.State
+    public typealias Action = UseCaseT.Action
+    let useCase: UseCaseT
+    var state: State
+    public var onNewState: (State) -> Void
+    let executorFactory: ExProducer
+    public init(useCase: UseCaseT, state: State, executorFactory: ExProducer, onNewState: @escaping (State) -> Void) {
+        self.useCase = useCase
+        self.state = state
         self.executorFactory = executorFactory
         self.onNewState = onNewState
     }
-    public func receive(_ action: LoginAction) {
+    public func receive(_ action: Action) {
         let (newState, effect) = useCase.receive(action, inState: state)
         state = newState
         onNewState(newState)
@@ -41,3 +55,5 @@ public class LoginOrchestrator {
         executorFactory.executorFor(effect: efct).execute(withOrchestrator: self)
     }
 }
+
+// Orchestrator --> UseCase, Executor --> State, Action
